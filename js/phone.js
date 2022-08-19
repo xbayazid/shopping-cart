09195f6314c3df6d0633342b628a6d5d254841df
xbayazid <bayazid9006@gmail.com>
2022-08-19 23:47:18 +0600

    phone function added

---
+++ js/phone.js
@@ -0,0 +1,30 @@
+function updatePhoneNumber(isIncrease){
+    const phoneNumberFiled = document.getElementById('phone-number-filed');
+    const phoneNumberString = phoneNumberFiled.value;
+    const previousPhoneNumber = parseFloat(phoneNumberString);
+    let newPhoneNumber;
+    if(isIncrease){
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else{
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+    phoneNumberFiled.value = newPhoneNumber;
+    return newPhoneNumber;
+}
+
+function updatePhoneTotalPrice(newPhoneNumber){
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+}
+
+document.getElementById('btn-phone-plus').addEventListener('click', function(){
+     const newPhoneNumber =  updatePhoneNumber(true);
+     updatePhoneTotalPrice(newPhoneNumber)
+});
+
+document.getElementById('btn-phone-minus').addEventListener('click', function(){
+    const newPhoneNumber = updatePhoneNumber(false);
+    updatePhoneTotalPrice(newPhoneNumber);
+});
