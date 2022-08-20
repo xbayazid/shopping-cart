4bedfbbc6db05b9a5e7e0f222acb5ab177f965dc
xbayazid <bayazid9006@gmail.com>
2022-08-20 13:02:56 +0600

    fully completed

--- js/phone.js
+++ js/phone.js
@@ -21,10 +21,12 @@ function updatePhoneTotalPrice(newPhoneNumber){
 
 document.getElementById('btn-phone-plus').addEventListener('click', function(){
      const newPhoneNumber =  updatePhoneNumber(true);
-     updatePhoneTotalPrice(newPhoneNumber)
+     updatePhoneTotalPrice(newPhoneNumber);
+     calculateSubTotal();
 });
 
 document.getElementById('btn-phone-minus').addEventListener('click', function(){
     const newPhoneNumber = updatePhoneNumber(false);
     updatePhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
 });
